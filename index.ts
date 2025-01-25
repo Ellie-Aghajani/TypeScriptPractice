d6d1cedc28c6f6d5ffb2418c162e6d3f9cae2086
Ellie-Aghajani <z.aqajani@gmail.com>
2025-01-25 13:37:25 -0800

    we can call fetch with type user or product and access their properties

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 //use interface for representing results of calling user/ product API endpoints
-
+//data is a user or product
 interface Result<T>{
     data: T | null;
     error: string | null; 
@@ -8,3 +8,15 @@ interface Result<T>{
 function fetch<T>(url : string) :Result<T>{
     return {data: null, error: null}
 }
+interface User{
+    username: string;
+}
+interface Product{
+    title: string;
+}
+
+let resultUser = fetch<User>('url');
+console.log(resultUser.data?.username);
+
+let resultProduct = fetch<Product>('url');
+console.log(resultProduct.data?.title);
